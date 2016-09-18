7d2314f956f68ee536871c6f234407222d2d84e0
azheng249 <andy.zheng249@gmail.com>
2016-09-18 16:38:44 -0400

    added instance method #todos to Category

--- todos/models.py
+++ todos/models.py
@@ -8,6 +8,9 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 
+    def todos(self):
+        return self.todo_set.all()
+
     class Meta:
         ordering = ('name',)
         verbose_name_plural = 'categories'
