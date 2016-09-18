113a911a3457abdd5eadf75074ef8f057e8656ba
azheng249 <andy.zheng249@gmail.com>
2016-09-18 16:11:17 -0400

    added instance methods and meta options to models. migrated changes

--- todos/models.py
+++ todos/models.py
@@ -5,7 +5,24 @@ from django.db import models
 class Category(models.Model):
     name = models.CharField(max_length=20)
 
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ('name',)
+        verbose_name_plural = 'categories'
+
 class Todo(models.Model):
     task = models.CharField(max_length=150)
     status = models.BooleanField(default=False)
     category = models.ManyToManyField(Category)
+
+    def __str__(self):
+        return self.task
+
+    def completed(self):
+        return self.status
+
+    class Meta:
+        ordering = ('task', 'status')
+        verbose_name_plural = 'todos'
