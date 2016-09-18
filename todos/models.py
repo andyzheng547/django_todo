74702f5ed12cc92165c51dce934a52099827b7fd
azheng249 <andy.zheng249@gmail.com>
2016-09-18 15:42:18 -0400

    created Category and Todo models in todos app. created migration file and migrated database with new models. added todos.apps.TodosConfig to django_todo settings

--- todos/models.py
+++ todos/models.py
@@ -2,4 +2,10 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=20)
+
+class Todo(models.Model):
+    task = models.CharField(max_length=150)
+    status = models.BooleanField(default=False)
+    category = models.ManyToManyField(Category)
