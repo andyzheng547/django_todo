5e478de6011cca59b4140a8a88435a186db4ab19
azheng249 <andy.zheng249@gmail.com>
2016-09-18 14:20:43 -0400

    remade todo app as todos. added todos index url

--- todos/views.py
+++ todos/views.py
@@ -1,3 +1,4 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def index(response):
+    return HttpResponse("<h1>This is the Todos homepage</h1>")
